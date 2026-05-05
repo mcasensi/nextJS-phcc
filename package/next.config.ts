ddd8639d1bc2e5c7ad08d79f914f95f8c81ce750
mcasensi <marj_444_bluepink@yahoo.com>
2026-05-06 00:25:57 +0800

    feat: upgrade next16

--- package/next.config.ts
+++ package/next.config.ts
@@ -1,13 +1,10 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    unoptimized: true,
-  },
-  eslint:{
-    ignoreDuringBuilds: true
-  }
-}
+    /* config options here */
+    images: {
+        unoptimized: true,
+    },
+};
 
-export default nextConfig
+export default nextConfig;
